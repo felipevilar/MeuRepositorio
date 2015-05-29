3c95a71806a745095b35c1d71e839990991d17be
felipevilar <felipvilar@gmail.com>
2015-05-29 17:08:28 -0400

    Update tabscreen.js

--- app/scenes/tabscreen.js
+++ app/scenes/tabscreen.js
@@ -10,7 +10,6 @@ function Scenetabscreen() {
 	
 	this.option = '';
 	this.isRotating = false;
-	this.lastDregree = 0;
 	
 };
 Scenetabscreen.prototype.initialize = function () {
@@ -18,13 +17,11 @@ Scenetabscreen.prototype.initialize = function () {
 	// this function will be called only once when the scene manager show this scene first time
 	// initialize the scene controls and styles, and initialize your variables here
 	// scene HTML and CSS will be loaded before this function is called
-	$('#tabuleiro').sfImage({
-		src:'images\\tabuleiro_x15_y150.png'
-	});
 };
 
 Scenetabscreen.prototype.handleShow = function (data) {
-	alert("Scenetabscreen.handleShow()"+this.numberPlayers);	
+	alert("Scenetabscreen.handleShow()"+this.numberPlayers);
+	$("#Scenetabscreen").css({'opacity':'1'});
 	// this function will be called when the scene manager show this scene
 };
 
@@ -48,9 +45,9 @@ Scenetabscreen.prototype.handleKeyDown = function (keyCode) {
 	// TODO : write an key event handler when this scene get focused
 	switch (keyCode) {
 		case sf.key.LEFT:
+			alert("this.isRotating: "+this.isRotating);
 			break;
 		case sf.key.RIGHT:
-			alert("#####");
 			break;
 		case sf.key.UP:
 			this.isRotating = false;
@@ -58,7 +55,7 @@ Scenetabscreen.prototype.handleKeyDown = function (keyCode) {
 		case sf.key.DOWN:
 			break;
 		case sf.key.ENTER:
-			this.onEnter()
+			this.onEnter();
 			break;
 		default:
 			alert("handle default key event, key code(" + keyCode + ")");
@@ -67,29 +64,43 @@ Scenetabscreen.prototype.handleKeyDown = function (keyCode) {
 };
 
 Scenetabscreen.prototype.onEnter = function() {
-	alert("Scenetabscreen.prototype.onEnter");
-
-	var text = document.getElementById("Text1").innerHTML = "100";
-
-	if(this.isRotating == false) {	
+	alert("Scenetabscreen.prototype.onEnter");	
+	
+	if(this.isRotating == false) {
 		
-		var randomNumber = Math.floor(Math.random() * 10) + 1;
+		this.isRotating = true;
+		
+		$('#roleta').css({'-webkit-transition-duration':'0s'});
+		document.getElementById("roleta").style.webkitTransform = "rotate(0deg)";
+		
+		var randomNumber = Math.floor(Math.random() * 6) + 1;
 		alert(randomNumber);
 		
-		document.getElementById("roleta").style.transitionDuration = "0s";
-		document.getElementById("roleta").style.webkitTransform = "rotate("+this.lastDregree+"deg)";
+		document.getElementById("roleta").style.webkitTransform = "rotate(0deg)";
 		
-		if(randomNumber == 1) {			
-			document.getElementById("roleta").style.transitionDuration = "3s";
-			document.getElementById("roleta").style.webkitTransform = "rotate(2266deg)";
-			this.isRotating = true;
-			this.lastDregree = -2266;
-		} else if(randomNumber == 2) {
-			document.getElementById("roleta").style.transitionDuration = "3s";
-			document.getElementById("roleta").style.webkitTransform = "rotate(2233deg)";
-			this.isRotating = true;
-			this.lastDregree = -2233;
+		if(randomNumber == 1) {
+			this.rotateStyle(30);
+		}else if(randomNumber == 2) {
+			this.rotateStyle(-30);
+		}else if(randomNumber == 3) {
+			this.rotateStyle(-90);
+		}else if(randomNumber == 4) {
+			this.rotateStyle(-150);
+		}else if(randomNumber == 5) {
+			this.rotateStyle(147);
+		} else if(randomNumber == 6) {
+			this.rotateStyle(90);
 		}
 	}
-	
 }
+
+
+Scenetabscreen.prototype.rotateStyle = function (rotateAngle) {
+	alert("Scenetabscreen.prototype.rotateStyle");
+	
+	var angle = 2160+rotateAngle;
+	$('#roleta').css({'-webkit-transition-duration':'5s'});
+	document.getElementById("roleta").style.webkitTransform = "rotate("+angle+"deg)";
+
+}
+
